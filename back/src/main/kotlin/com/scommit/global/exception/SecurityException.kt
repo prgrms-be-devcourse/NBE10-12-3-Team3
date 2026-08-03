165938cce4c190f39ff313a334e7603df8e41295
JUNSEO OH <piker0925@gmail.com>
2026-08-03 11:44:56 +0900

    refactor: Global 코틀린 나머지 마이그레이션

---
+++ back/src/main/kotlin/com/scommit/global/exception/SecurityException.kt
@@ -0,0 +1,12 @@
+package com.scommit.global.exception
+
+import com.scommit.global.dto.RsData
+
+// TODO: BuisinessException과 통합 또는 다른 방법 강구
+// Exception을 14183과 분리하기 위한 임시 예외 클래스입니다.
+class SecurityException(
+    private val resultCode: String,
+    private val msg: String,
+) : RuntimeException("$resultCode : $msg") {
+    fun getRsData(): RsData<Void> = RsData(resultCode, msg, null)
+}
